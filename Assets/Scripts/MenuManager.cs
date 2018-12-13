fe75805ae5022610461058b858a944b934bd3aca
faruk hadzigrahic <faruk.hadzigrahic@elev.ga.lbs.se>
2018-12-14 00:11:03 +0100

    GameMenu

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    //variabel som ersätter "Turtorial" (tutorial igentligen men jag skrev fel och har inte tid att ändra) till levelToLoad
+    public string levelToLoad = "Turtorial";
+
+
+    //public void med namnet "ToGame" laddar levelToLoad, I detta fallet "Turtorial"
+    public void ToGame()
+    {
+    SceneManager.LoadScene(levelToLoad);
+    }
+}
